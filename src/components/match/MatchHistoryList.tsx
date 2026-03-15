d64fd2270c516b696e364eabefa276f993c0b3e3
Patrick Seeman <patseem189@gmail.com>
2026-03-15 19:26:51 -0400

    change the way scrollbar works

--- src/components/match/MatchHistoryList.tsx
+++ src/components/match/MatchHistoryList.tsx
@@ -22,9 +22,23 @@ export default function MatchHistoryList({
 
   return (
     <div className="space-y-6">
-      {/* Horizontal scroll */}
-      <div className="overflow-x-auto pb-4 scrollbar-thin scrollbar-thumb-gray-700 scrollbar-track-gray-900">
-        <div className="flex gap-6 px-2">
+      {/* Horizontal scroll – modern subtle style */}
+      <div
+        className={`
+          overflow-x-auto pb-6
+          scrollbar-gutter-stable               // prevents layout shift (modern browsers)
+          [&::-webkit-scrollbar]:h-3            // very thin
+          [&::-webkit-scrollbar-track]:bg-transparent
+          [&::-webkit-scrollbar-thumb]:bg-neutral-600/40
+          [&::-webkit-scrollbar-thumb]:rounded-xl
+          [&::-webkit-scrollbar-thumb]:transition-all
+          [&::-webkit-scrollbar-thumb]:duration-200
+          hover:[&::-webkit-scrollbar-thumb]:bg-neutral-500/70
+          active:[&::-webkit-scrollbar-thumb]:bg-sky-600/80
+          [&::-webkit-scrollbar-corner]:bg-transparent
+        `}
+      >
+        <div className="flex gap-6 px-2 min-w-max">
           {matches.map((match) => (
             <MatchCard
               key={match.match_id}
