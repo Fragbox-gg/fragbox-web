953e80da353cf862899ea2afa479940ee80b5bb8
Patrick Seeman <patseem189@gmail.com>
2026-04-05 18:07:57 -0400

    add base app id in properly for verification purposes with base.dev

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,39 +1,39 @@
 import "../styles/globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
 import { darkTheme, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 
 import { config } from "../wagmi";
-import { Metadata } from "next";
 
 const client = new QueryClient();
 
-export const metadata: Metadata = {
-  other: {
-    "base:app_id": "69d2da5b759b9a105ccd8327",
-  },
-};
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={client}>
-        <RainbowKitProvider
-          theme={darkTheme({
-            accentColor: "#32CD32",
-            accentColorForeground: "black",
-            borderRadius: "large",
-            fontStack: "system",
-            overlayBlur: "small",
-          })}
-        >
-          <Component {...pageProps} />
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+    <>
+      <Head>
+        <meta name="base:app_id" content="69d2da5b759b9a105ccd8327" />
+      </Head>
+
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={client}>
+          <RainbowKitProvider
+            theme={darkTheme({
+              accentColor: "#32CD32",
+              accentColorForeground: "black",
+              borderRadius: "large",
+              fontStack: "system",
+              overlayBlur: "small",
+            })}
+          >
+            <Component {...pageProps} />
+          </RainbowKitProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </>
   );
 }
 
